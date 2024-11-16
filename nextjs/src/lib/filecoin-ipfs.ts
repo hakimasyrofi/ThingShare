43d174b70b9b9cd7ecb62456f90f3c1fd4da3270
hakimasyrofi <hakimasyrofi@gmail.com>
2024-11-16 10:14:40 +0700

    init filecoin lighthouse and shadcn ui label and text area

---
+++ nextjs/src/lib/filecoin-ipfs.ts
@@ -0,0 +1,32 @@
+import lighthouse from "@lighthouse-web3/sdk";
+
+/**
+ * This function allows you to upload a file or a folder to Lighthouse.
+ *
+ * @param {string} path - The location of your file or folder.
+ * @param {string} apiKey - Your personal API key for Lighthouse.
+ * @param {object} dealParameters - Custom parameters for file storage deals(check FVM section).
+ * @return {object} - Returns details about the uploaded file.
+ */
+const lighthouseApiKey = process.env.LIGHTHOUSE_API_KEY || "";
+
+async function uploadToLighthouse(file: File) {
+  try {
+    const uploadResponse = await lighthouse.upload(file, lighthouseApiKey);
+    console.log(uploadResponse);
+    return uploadResponse;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+}
+
+/*Sample response
+{
+  data: {
+    Name: 'wow.jpg',
+    Hash: 'QmUHDKv3NNL1mrg4NTW4WwJqetzwZbGNitdjr2G6Z5Xe6s',
+    Size: '31735'
+  }
+}
+*/
